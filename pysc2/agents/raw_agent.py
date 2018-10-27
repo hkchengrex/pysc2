529175212c00e3c34e9136e1a2482c7089cc94f3
Rex Cheng <holyrexcheng@gmail.com>
2018-10-28 01:55:57 +0800

    FUNCTIONS cannot read key

---
+++ pysc2/agents/raw_agent.py
@@ -0,0 +1,53 @@
+# Copyright 2017 Google Inc. All Rights Reserved.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS-IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+"""Scripted agents."""
+
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import numpy
+
+from pysc2.agents import base_agent
+from pysc2.lib import actions
+from pysc2.lib import features
+
+_PLAYER_SELF = features.PlayerRelative.SELF
+_PLAYER_NEUTRAL = features.PlayerRelative.NEUTRAL  # beacon/minerals
+_PLAYER_ENEMY = features.PlayerRelative.ENEMY
+
+FUNCTIONS = actions.FUNCTIONS
+
+def _xy_locs(mask):
+  """Mask should be a set of bools from comparison with a feature layer."""
+  y, x = mask.nonzero()
+  return list(zip(x, y))
+
+class MoveToBeacon(base_agent.BaseAgent):
+  """An agent specifically for solving the MoveToBeacon map."""
+
+  def step(self, obs):
+    super(MoveToBeacon, self).step(obs)
+
+    return FUNCTIONS.Move_raw("now", (0, 0))
+
+    if FUNCTIONS.Move_raw.id in obs.observation.available_actions:
+      player_relative = obs.observation.feature_screen.player_relative
+      beacon = _xy_locs(player_relative == _PLAYER_NEUTRAL)
+      if not beacon:
+        return FUNCTIONS.no_op()
+      beacon_center = numpy.mean(beacon, axis=0).round()
+      return FUNCTIONS.Move_raw("now", beacon_center)
+    else:
+      return FUNCTIONS.select_army("select")
